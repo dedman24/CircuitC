0794fbb311649bbfa9ef0a7e1d08e4d0269b945d
dedmanwalking <dedmanwalking@proton.me>
2025-12-25 21:08:59 +0000

    Add files via upload

---
+++ CircuitC_compiler/lexer/NOAHZK_bigint_lib/noahzk_bigint.h
@@ -0,0 +1,28 @@
+/*
+   NOAHZK_bigint reference source code package - reference C implementations
+
+   Copyright 2025, dedmanwalking <dedmanwalking@proton.me>.  You may use this under the
+   terms of the CC0 1.0 Universal license, linked below:
+   - CC0 1.0 Universal : http://creativecommons.org/publicdomain/zero/1.0
+*/
+
+#ifndef NOAHZK_bigint_included
+#define NOAHZK_bigint_included
+
+#include "ops/definitions.h"
+#include "ops/logarithms.h"
+#include "ops/type.h"
+#include "ops/add.h"
+#include "ops/mul.h"
+#include "ops/sub.h"
+
+// NAMING SCHEME:
+//      NOAHZK_variable_width_<op>
+//      for arithmetic ops, the pointers to the source arguments are named:
+// 1  arg  ~ src
+// 2+ args ~ rs0, ..., rsn
+//      multiple dsts are named as:
+// 1  dst  ~ dst
+// 2+ dsts ~ dst0, ..., dstn
+
+#endif
